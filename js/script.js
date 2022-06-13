f0a36332dbacbac976da779403c3722c8a5a9a85
Toufiq Hasan Kiron <toufiqhasankiron2@gmail.com>
2022-06-13 14:01:58 +0600

    preloader added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+/* ------------- Preloader --------------- */
+window.addEventListener("load", ()=>{ 
+  document.querySelector(".main").classList.remove("hidden");
+  document.querySelector(".home-section").classList.add("active");
+  document.querySelector(".page-loader").classList.add("fade-out");
+  setTimeout(()=>{
+    document.querySelector(".page-loader").style.display = "none";
+  } , 1000);
+});
+
+
 /* ------------- Toggle Navbar --------------- */
 const navToggler = document.querySelector(".nav-toggler");
 navToggler.addEventListener("click", () => {
